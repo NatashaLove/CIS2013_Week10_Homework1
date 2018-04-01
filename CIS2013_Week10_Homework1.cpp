c99aa3ee718551723140cc71cee024a5ae7672f9
Natasha S Love <debora@inbox.ru>
2018-04-01 13:37:07 -0500

    debug happened

--- CIS2013_Week10_Homework1.cpp
+++ CIS2013_Week10_Homework1.cpp
@@ -12,7 +12,7 @@ int main( )
 // int board [x][y];
  int mine=0;
  
-	cout << '/t';
+	cout << ' ' << endl;
 	cout << "Enter the board size :\n";
 	cout<< "x =";
 	cin >> x;
@@ -47,7 +47,7 @@ int main( )
 	cout << "Enter amount of mines (not more than 10)for your field: " << endl;
 	cin >> mine;
 	cout << endl;
-	cout << "Planting mines..."; 
+	cout << mine << " mines are..."; 
 
 	int* mines = new int[mine*2];
 	int mx, my;
@@ -75,18 +75,18 @@ int main( )
 		 mines[n + 1] = my;
 		
 	}
-	cout << " ...Done" << endl; 
-
-	// Попытка напечатать игровое поле:
+	cout << " ...planted in the play board" << endl; 
+	cout << endl;
+	//  Printing the play board:
 
 	print(m, x, y);
 	
-	// координаты мин- игрок вводить должен через cin >> x >> y
+	// then the player inserets the coordinates through cin>> x >> y//  координаты мин- игрок вводить должен через cin >> x >> y
 	// и сравнивать их с mines !!! (array)
-	// mines[0], mines[1] - первая мина, 2-3, 4-5 и т.д
-	// это ж просто поле - которое печатаем - m? и передаем туда новые значения после каждого хода
-	// а мины хранятся в mines
-	// сравниваем ходы с mines - и результат печатаем через m
+	// mines[0], mines[1] - is the first mine, 2-3, 4-5 etc.
+	// m - is just printing field - which is updated after every move// это просто поле - которое печатаем - m- и передаем туда новые значения после каждого хода
+	// the mines are stored in mines array //а мины хранятся в mines
+	// every move is compared to mines array and then printed with m array// сравниваем ходы с mines - и результат печатаем через m
 	
 	
 	while (1) {
@@ -103,25 +103,29 @@ int main( )
 		 cin >> row;
 		 cout << endl; 
 		 }
-		  unsigned char bOver = 0; 
+		  unsigned char gOver = 0; 
 		  for (int n = 0; n < mine * 2; n += 2) {
 			  
 			   if (mines[n] == column && mines[n + 1] == row) { 
 					
 					//show all mines
 					for (int k = 0; k < mine * 2; k += 2) { 
-					m[mines[k+1]][mines[k]] = 'M'; 
+					m[mines[k+1]][mines[k]] = 'X'; // mark hidden mines
 					}
-					m[mines[n]][mines[n + 1]] = 'X';//mark badluck especially 
+					m[mines[n]][mines[n + 1]] = '@';//mark exploded mine
 					
 					print(m, x, y); 
 					
-					cout << "Game over!"; 
-					bOver = 1; 
+					cout << endl;
+					
+					cout << ' ' << "Game over!"; 
+					cout<< endl;
+					
+					gOver = 1; 
 				} 
 			} 
 			
-			 if (bOver) 
+			 if (gOver) 
 				 break; 
 			 else { 
 			 
@@ -143,7 +147,7 @@ int main( )
  void print(unsigned char** board, int x, int y)
 {
 
-	cout << '\t';
+	cout << '	';
 	for (int i = 0; i < x;i++)
 	{
 		cout << i << ' ';
@@ -151,12 +155,13 @@ int main( )
 	cout << '\n';
 	for (int i = 0; i < y; i++)
 	{
-		cout << i << '\t';
+		cout << i << '	';
 		for (int j = 0; j < x;j++)
 		{
 			cout << board[i][j] << ' ';
 		}
 		cout << '\n';
 	}
+	cout<< endl;
 }
  
